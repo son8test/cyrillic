f5c44e5ed68da358cc12c5ddfa6f3a9dc45e10ab
Oleg'Ease'Kharchuk <olegease@gmail.com>
2025-10-30 00:11:12 +0200

    fix testing encoded error none

--- testing/encoded/error/none.cxx
+++ testing/encoded/error/none.cxx
@@ -1,10 +1,13 @@
 #include <son8/cyrillic/encoded.hxx>
 #include <son8/cyrillic/state.hxx>
 
+#include <cassert>
+
 using namespace son8::cyrillic;
 
 int main( ) {
     this_thread::state( Language::Russian );
-    Encoded test{ u"test" };
+    Encoded test{ u"тест" };
+    return not ( test.out( ) == "test" );
 }
 
